a8cb3bf65e60efa1462536951dd71e6cea5c42d9
gui <149209763+guirdgrs@users.noreply.github.com>
2023-10-27 20:57:42 -0300

    Interface e Código concluídos
    A interface e o código foram finalizados juntamente com um tratamento de erro simples.

--- AreaTrianguloForms/Form1.Designer.cs
+++ AreaTrianguloForms/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txbNum1 = new System.Windows.Forms.TextBox();
+            this.txbNum2 = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbNum1
+            // 
+            this.txbNum1.Location = new System.Drawing.Point(85, 74);
+            this.txbNum1.Name = "txbNum1";
+            this.txbNum1.Size = new System.Drawing.Size(100, 20);
+            this.txbNum1.TabIndex = 0;
+            // 
+            // txbNum2
+            // 
+            this.txbNum2.Location = new System.Drawing.Point(85, 100);
+            this.txbNum2.Name = "txbNum2";
+            this.txbNum2.Size = new System.Drawing.Size(100, 20);
+            this.txbNum2.TabIndex = 1;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(41, 77);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(31, 13);
+            this.lblNum1.TabIndex = 2;
+            this.lblNum1.Text = "Base";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(41, 107);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(34, 13);
+            this.lblNum2.TabIndex = 3;
+            this.lblNum2.Text = "Altura";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(59, 153);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(155, 24);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(82, 190);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(107, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Preencha os campos";
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbResultado.Enabled = false;
+            this.txbResultado.Location = new System.Drawing.Point(85, 215);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(100, 13);
+            this.txbResultado.TabIndex = 6;
+            this.txbResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(260, 277);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.txbNum2);
+            this.Controls.Add(this.txbNum1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Área do Triângulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txbNum1;
+        private System.Windows.Forms.TextBox txbNum2;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txbResultado;
     }
 }
 
--- AreaTrianguloForms/Form1.cs
+++ AreaTrianguloForms/Form1.cs
@@ -16,5 +16,37 @@ namespace AreaTrianguloForms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Var para armazenar a base e a altura
+                double num1, num2;
+
+                //Preenchendo var com o TextBox
+                num1 = double.Parse(txbNum1.Text); //Base
+                num2 = double.Parse(txbNum2.Text); //Altura
+
+                if (num1 == 0 || num2 == 0) //Condição - Valores não podem ser zero
+                {
+                    MessageBox.Show("Valores não podem ser zero");
+                    txbResultado.Text = "Erro";
+                }
+                else //Área do triângulo = b * h / 2
+                {
+                    lblResultado.Text = "A área do triângulo é: ";
+                    txbResultado.Text = (num1 * num2 / 2).ToString(); //Resultado
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Erro! Dados inválidos");
+                lblResultado.Text = "PREENCHA OS CAMPOS";
+                txbResultado.Clear();
+                txbNum1.Clear();
+                txbNum2.Clear();
+            }
+        }
     }
 }
